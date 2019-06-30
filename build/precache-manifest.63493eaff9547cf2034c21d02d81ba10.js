6d7e308a50a0c0bf709479f59e4c4512986b29d5
KoolJ <koolj.net@gmail.com>
2019-07-01 04:05:07 +0700

    Add files via upload

---
+++ build/precache-manifest.63493eaff9547cf2034c21d02d81ba10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347baf212c8bf584a9fc63c261dd0553",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b4b29114baba29136b7",
+    "url": "/static/css/2.e25306ee.chunk.css"
+  },
+  {
+    "revision": "e9f1f78f74d04ade914a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7b4b29114baba29136b7",
+    "url": "/static/js/2.4c7e55b1.chunk.js"
+  },
+  {
+    "revision": "e9f1f78f74d04ade914a",
+    "url": "/static/js/main.2927d47f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "00c5ce7701d89a6bced9dd50438b0786",
+    "url": "/static/media/logo_90.00c5ce77.png"
+  }
+]);
